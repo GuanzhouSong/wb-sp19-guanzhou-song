98a7ca83cbd3393e169f9c54eee563488cef6137
Guanzhou Song <guanzhou.song@gmail.com>
2019-01-25 09:57:18 +0800

    initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import WhiteBoard from "./containers/WhiteBoard";
+
+ReactDOM.render(
+    <div className="container-fluid">
+        <WhiteBoard/>
+    </div>,
+    document.getElementById("root")
+);
