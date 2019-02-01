3e6f990292f1f1bea20e4c97da2c78a3f94fc628
Guanzhou Song <guanzhou.song@gmail.com>
2019-02-01 22:23:23 +0800

    temp delete

--- src/components/Widgets/ImageWidget.js
+++ src/components/Widgets/ImageWidget.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
-const ImageWidget = () =>
-    <h2>Image Widget</h2>;
+const ImageWidget = ({widget, updateWidget}) =>
+    <div>
+      <h2>Heading Widget</h2>
+
+      <input
+          value={widget.text}
+          onChange={event => {
+            widget.text = event.target.value;
+            updateWidget(widget)
+          }}
+          className="form-control"/>
+
+      <h3>Preview</h3>
+      {
+        widget.size === 1 && <h1>{widget.text}</h1> ||
+        widget.size === 2 && <h2>{widget.text}</h2> ||
+        widget.size === 3 && <h3>{widget.text}</h3> ||
+        widget.size === 4 && <h4>{widget.text}</h4> ||
+        widget.size === 5 && <h5>{widget.text}</h5>
+      }
+    </div>;
 
 export default ImageWidget
